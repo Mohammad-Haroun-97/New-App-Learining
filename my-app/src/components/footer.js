d15f60a31c56baf207541eabec5568a19c06d49c
Mohammad-Haroun-97 <harounmohammad@yahoo.com>
2021-09-03 14:37:02 +0200

    finish lab01

---
+++ my-app/src/components/footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class Footer extends React.Component{
+
+    render(){
+
+        return(
+
+            <>
+
+            <h1>Mohammad Haroun</h1>
+
+
+
+            </>
+        );
+    }
+}
+
+export default Footer
